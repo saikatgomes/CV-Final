1e43c77e22cd20eda22fef364eee60914ea92009
saikatgomes <saikat@cs.wisc.com>
2014-04-24 16:32:38 -0500

    fix missing code issue

--- code/test/core/main.m
+++ code/test/core/main.m
@@ -42,7 +42,7 @@ function multiObjectTracking()
 
     while ~isDone(playerDetector.reader)
 
-        frame = obj.reader.step();
+        frame = playerDetector.reader.step();
         [centroids, bboxes, mask] = detectObjects(frame);
         predictNewLocationsOfTracks();
         [assignments, unassignedTracks, unassignedDetections] = ...
@@ -57,6 +57,19 @@ function multiObjectTracking()
 
     end
     
+    function [centroids, bboxes, mask] = detectObjects(frame)
+        
+        % Detect foreground.
+        mask = obj.detector.step(frame);
+        
+        % Apply morphological operations to remove noise and fill in holes.
+        mask = imopen(mask, strel('rectangle', [3,3]));
+        mask = imclose(mask, strel('rectangle', [15, 15])); 
+        mask = imfill(mask, 'holes');
+        
+        % Perform blob analysis to find connected components.
+        [~, centroids, bboxes] = obj.blobAnalyser.step(mask);
+    end
     
 
     function predictNewLocationsOfTracks()
