bef7b78a4abfb7a7d7e6f72fbe495652d1a1cd07
saikatgomes <saikat@cs.wisc.com>
2014-04-25 01:36:42 -0500

    trying k-means

---
+++ code/test/kmean/testKMeans.m
@@ -0,0 +1,23 @@
+%rng('default') % For reproducibility
+X = [randn(100,2)+ones(100,2);...
+    randn(100,2)-ones(100,2)];
+%X=[ 4 4 ; 3 3 ; 2 2; 5 1; 11 3; 12 5; 10 7];
+opts = statset('Display','final');
+[idx,ctrs] = kmeans(X,4,'Distance','city',...
+    'Replicates',5,'Options',opts);
+plot(X(idx==1,1),X(idx==1,2),'r.','MarkerSize',12)
+hold on
+plot(X(idx==2,1),X(idx==2,2),'b.','MarkerSize',12)
+hold on
+plot(X(idx==3,1),X(idx==3,2),'g.','MarkerSize',12)
+hold on
+plot(X(idx==4,1),X(idx==4,2),'c.','MarkerSize',12)
+
+plot(ctrs(:,1),ctrs(:,2),'kx',...
+    'MarkerSize',12,'LineWidth',2)
+plot(ctrs(:,1),ctrs(:,2),'ko',...
+    'MarkerSize',12,'LineWidth',2)
+
+legend('Cluster 1','Cluster 2','Centroids',...
+    'Location','NW')
+hold off
