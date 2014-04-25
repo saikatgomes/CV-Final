e491ca95d3c63b7e3d343852f923840c0f4904a1
saikatgomes <saikat@cs.wisc.com>
2014-04-24 21:07:51 -0500

    create average background images

---
+++ code/test/bgTrain/aveBackground.m
@@ -0,0 +1,12 @@
+D = vision.VideoFileReader('../data/packers_A/1.mp4');
+c=1;
+frame = D.step();
+sum=double(zeros(size(frame,1),size(frame,2),3));
+while ~isDone(D)
+   frame = D.step();
+   c=c+1;
+   sum=sum+frame;
+end
+ave=sum/c;
+imshow(double(ave));
+imwrite(double(ave),'../data/packers_A/1bg.jpg');
