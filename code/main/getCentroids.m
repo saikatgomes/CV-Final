f6bb20493c94265ed385da68de086bfaf9de3405
saikatgomes <saikat@cs.wisc.com>
2014-04-27 14:32:57 -0500

    make k-means better

--- code/main/getCentroids.m
+++ code/main/getCentroids.m
@@ -2,7 +2,11 @@ function [ idx,ctrs ,SUMD, DistMat ] = getCentroids( X, Y, k )
     
 dataset=[X Y];
 opts = statset('Display','final');
-[idx,ctrs,SUMD, DistMat ] = kmeans(dataset,k,'Distance','city',...
+% [idx,ctrs,SUMD, DistMat ] = kmeans(dataset,k,'Distance','city',...
+%     'Replicates',5,'Options',opts);
+
+[idx,ctrs,SUMD, DistMat ] = kmeans(dataset,k,'Distance','sqEuclidean',...
     'Replicates',5,'Options',opts);
+
 end
 
