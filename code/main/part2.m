edf8032c34164d68c263e9edcaa34babea12a991
saikatgomes <saikat@cs.wisc.com>
2014-04-27 18:30:31 -0500

    create part 2 of the processing pipeline: begin work on mostion estimation and tracking individual players

---
+++ code/main/part2.m
@@ -0,0 +1,6 @@
+
+clear
+load('../../sandbox/data99/data_test/X_new.mat')
+load('../../sandbox/data99/data_test/Y_new.mat')
+load('../../sandbox/data99/data_test/X.mat')
+load('../../sandbox/data99/data_test/Y.mat')
