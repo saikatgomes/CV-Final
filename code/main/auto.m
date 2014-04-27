da45a3a7674cad2a349ac2b35d06b031ef6310cd
saikatgomes <saikat@cs.wisc.com>
2014-04-26 23:50:57 -0500

    centoid prediction is more accurate

--- code/main/auto.m
+++ code/main/auto.m
@@ -13,7 +13,7 @@ props.setProperty('mail.smtp.socketFactory.class', ...
     'javax.net.ssl.SSLSocketFactory');
 props.setProperty('mail.smtp.socketFactory.port','465');
 
-mainDir='../../data/';
+mainDir='../../sandbox/data99/';
 hostName=getHostName();
 
 d = dir(mainDir);
@@ -28,18 +28,18 @@ for i=1:length(folderName)
     
     for j=1:length(fileName)
         fullFileName=strcat(mainDir,folderName{i},'/',fileName{j});
-        lockFileName=strcat(fullFileName,'.LOCK');
-        if(exist(lockFileName,'file')==2)
-            display(strcat(datestr(now,'HH:MM:SS'),' [INFO] Skipping >[',fullFileName,'] as lock file found' ));
-            continue;
-        else
-            fclose(fopen(lockFileName, 'w'));
-        end
+%         lockFileName=strcat(fullFileName,'.LOCK');
+%         if(exist(lockFileName,'file')==2)
+%             display(strcat(datestr(now,'HH:MM:SS'),' [INFO] Skipping >[',fullFileName,'] as lock file found' ));
+%             continue;
+%         else
+%             fclose(fopen(lockFileName, 'w'));
+%         end
         display(strcat(datestr(now,'HH:MM:SS'),' [INFO] proccessing >',fullFileName));
         fName=fullFileName(1:length(fullFileName)-4);
         try
             startTime=datestr(now,'HH:MM:SS');
-            process(fName,'mp4');
+            process(fName,'mp4',1,'../lib/extrema');
             delete(lockFileName);
             endTime=datestr(now,'HH:MM:SS');
             sendmail('saikatgomes@gmail.com', 'TESTBOT: Success', ...
--- code/main/autoSpc.m
+++ code/main/autoSpc.m
@@ -1,64 +1,62 @@
-function [] = autoSpc( mainDir )
+% autoSpc('data4/','../data/');
 
+function [] = autoSpc( mainDir ,outDir)
+    myaddress = 'cs766vision@gmail.com';
+    mypassword = 'moneyball123';
 
+    setpref('Internet','E_mail',myaddress);
+    setpref('Internet','SMTP_Server','smtp.gmail.com');
+    setpref('Internet','SMTP_Username',myaddress);
+    setpref('Internet','SMTP_Password',mypassword);
 
-myaddress = 'cs766vision@gmail.com';
-mypassword = 'moneyball123';
+    props = java.lang.System.getProperties;
+    props.setProperty('mail.smtp.auth','true');
+    props.setProperty('mail.smtp.socketFactory.class', ...
+        'javax.net.ssl.SSLSocketFactory');
+    props.setProperty('mail.smtp.socketFactory.port','465');
 
-setpref('Internet','E_mail',myaddress);
-setpref('Internet','SMTP_Server','smtp.gmail.com');
-setpref('Internet','SMTP_Username',myaddress);
-setpref('Internet','SMTP_Password',mypassword);
+    %mainDir='../../data/';
+    hostName=getHostName();
 
-props = java.lang.System.getProperties;
-props.setProperty('mail.smtp.auth','true');
-props.setProperty('mail.smtp.socketFactory.class', ...
-    'javax.net.ssl.SSLSocketFactory');
-props.setProperty('mail.smtp.socketFactory.port','465');
+    d = dir(mainDir);
+    isub = [d(:).isdir];
+    folderName = {d(isub).name}';
+    folderName(ismember(folderName,{'.','..'})) = [];
 
-%mainDir='../../data/';
-hostName=getHostName();
+    for i=1:length(folderName)
+        fPath=fullfile(strcat(mainDir,folderName{i}),'/*mp4');
+        fileName  =  dir(fPath);
+        fileName  = {fileName.name}';
 
-d = dir(mainDir);
-isub = [d(:).isdir];
-folderName = {d(isub).name}';
-folderName(ismember(folderName,{'.','..'})) = [];
+        for j=1:length(fileName)
+            fullFileName=strcat(mainDir,folderName{i},'/',fileName{j});
+            lockFileName=strcat(fullFileName,'.LOCK');
+            if(exist(lockFileName,'file')==2)
+                display(strcat(datestr(now,'HH:MM:SS'),' [INFO] Skipping >[',fullFileName,'] as lock file found' ));
+                continue;
+            else
+                fclose(fopen(lockFileName, 'w'));
+            end
+            display(strcat(datestr(now,'HH:MM:SS'),' [INFO] proccessing >',fullFileName));
+            fName=fullFileName(1:length(fullFileName)-4);
+            try
+                startTime=datestr(now,'HH:MM:SS');
+                process(fName,'mp4',0);
+                delete(lockFileName);
+                endTime=datestr(now,'HH:MM:SS');
+                sendmail('saikatgomes@gmail.com', 'TESTBOT: Success', ...
+                    strcat('Success: ',fullFileName,' @ ',hostName,' ____ START:',startTime,' _____ END:',endTime));
+            catch ME   
+                mkdir(strcat('error/',folderName{i},'/'));
+                movefile(fullFileName,strcat('error/',folderName{i},'/',fileName{j}));
+                delete(lockFileName);
+                sendmail('saikatgomes@gmail.com', 'TESTBOT: Fail', strcat('error: ', ...
+                    ME.message,'.......... iden: ',ME.identifier,' ............... file',fullFileName,' @ ',hostName));
+                display(strcat(datestr(now,'HH:MM:SS'),' [ERROR] FAILED proccessing ',fullFileName));
+            end
+        end    
+        movefile(strcat(mainDir,folderName{i}),outDir,'f');
 
-for i=1:length(folderName)
-    fPath=fullfile(strcat(mainDir,folderName{i}),'/*mp4');
-    fileName  =  dir(fPath);
-    fileName  = {fileName.name}';
-    
-    for j=1:length(fileName)
-        fullFileName=strcat(mainDir,folderName{i},'/',fileName{j});
-        lockFileName=strcat(fullFileName,'.LOCK');
-        if(exist(lockFileName,'file')==2)
-            display(strcat(datestr(now,'HH:MM:SS'),' [INFO] Skipping >[',fullFileName,'] as lock file found' ));
-            continue;
-        else
-            fclose(fopen(lockFileName, 'w'));
-        end
-        display(strcat(datestr(now,'HH:MM:SS'),' [INFO] proccessing >',fullFileName));
-        fName=fullFileName(1:length(fullFileName)-4);
-        try
-            startTime=datestr(now,'HH:MM:SS');
-            process(fName,'mp4');
-            delete(lockFileName);
-            endTime=datestr(now,'HH:MM:SS');
-            sendmail('saikatgomes@gmail.com', 'TESTBOT: Success', ...
-                strcat('Success: ',fullFileName,' @ ',hostName,' ____ START:',startTime,' _____ END:',endTime));
-        catch ME
-            sendmail('saikatgomes@gmail.com', 'TESTBOT: Fail', strcat('error: ', ...
-                ME.message,'.......... iden: ',ME.identifier,' ............... file',fullFileName,' @ ',hostName));
-            display(strcat(datestr(now,'HH:MM:SS'),' [ERROR] FAILED proccessing ',fullFileName));
-        end
     end
-    
-    
-end
-
-
-
-
 end
 
