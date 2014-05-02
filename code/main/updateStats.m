2b9aa17821df328e20c44b39842e864d23777d3a
saikatgomes <saikat@cs.wisc.com>
2014-05-01 19:05:20 -0500

    calculate place distance travelled

--- code/main/updateStats.m
+++ code/main/updateStats.m
@@ -1,7 +1,17 @@
-function [ ] = updateStats( playerCollection )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [playerCollection ] = updateStats( playerCollection )
 
+    for i=1:playerCollection.count
+        
+        onePlayer=playerCollection.list(i);
+        steps = onePlayer.steps;
+        distance=0;
+        for t=1:steps-1
+            d=pdist([onePlayer.trackY_net(t) onePlayer.trackX_net(t); ...
+                onePlayer.trackY_net(t+1) onePlayer.trackX_net(t+1)]);
+            distance=distance+d;
+        end
+        onePlayer.distance=distance;
+    end
 
 end
 
