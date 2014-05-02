3c30cb81c709e94d5d10101f070fe734a4d6c2d9
saikatgomes <saikat@cs.wisc.com>
2014-05-02 00:37:03 -0500

    continously display the tracks picked up

--- code/main/displayTracksContinuousOneByOne.m
+++ code/main/displayTracksContinuousOneByOne.m
@@ -21,20 +21,26 @@ function [  ] = displayTracksContinuous( playerCollection, frame )
     totNumOfFrame=playerCollection.totNumOfFrame;
     count=playerCollection.count ;
         
-    for j=1:totNumOfFrame        
-        imshow(frame);
-    
+    for i=1:count
+        onePlayer=playerCollection.list(i);
+        st=onePlayer.startFrame;
+        last=onePlayer.lastFrame;
+        Cz = mod(i,6)+1; %pick color
+        clear f;
         hold on;
-        for i=1:count
-            onePlayer=playerCollection.list(i);
-            st=onePlayer.startFrame;
-            last=onePlayer.lastFrame;
-            Cz = mod(i,6)+1; %pick color
-            if(st>j || last<j)
+        for j=1:totNumOfFrame
+            if(st>j)
                 continue;
             end
+            if(last<j)
+                break;
+            end
             %pause(.1);
-            plot(onePlayer.smoothTrackY(j,1),...
+            if(j>1 && st<j)
+               delete(f(j-1)); 
+            end
+            j
+            f(j)=plot(onePlayer.smoothTrackY(j,1),...
                 onePlayer.smoothTrackX(j,1),...
                 'o','markersize',12,'linewidth',8,'Color',c_list(Cz));
             plot(onePlayer.smoothTrackY(st:j,1),...
@@ -42,20 +48,9 @@ function [  ] = displayTracksContinuous( playerCollection, frame )
                 'w.-','markersize',1,'linewidth',1);
             
         end
-        
-        
-        plot([w*.33 w*.33]',[1 h]','y.:','markersize',1,'linewidth',1)
-        plot([w*.66 w*.66]',[1 h]','y.:','markersize',1,'linewidth',1)
-
-        plot([w*.4 w*.4]',[1 h]','r.:','markersize',1,'linewidth',1)
-        plot([w*.59 w*.59]',[1 h]','r.:','markersize',1,'linewidth',1)
-
-        plot([1 w]',[h*.64 h*.64]','y.:','markersize',1,'linewidth',1)
-        
-        hold off;
-        pause(.01);
     end
     
 
+        hold off;
 end
 
