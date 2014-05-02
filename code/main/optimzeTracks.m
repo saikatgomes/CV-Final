f2b7449f7d2ebde4a6a1f04e07582e54e065ccbb
saikatgomes <saikat@cs.wisc.com>
2014-05-02 12:54:24 -0500

    recursively optimize the tracks

--- code/main/optimzeTracks.m
+++ code/main/optimzeTracks.m
@@ -1,5 +1,35 @@
-function [  ] = optimzeTracks( playerCollection )
-
-
+function [ isFound ] = optimzeTracks( playerCollection )
+    
+    isFound=0;
+    minDist=[25 50 60 70 75];
+    timeDiff=[0 1 2 3 4 5 -1 -2 -3];
+    pCount=playerCollection.count;
+    for d=minDist
+        for t=timeDiff
+            for p=1:pCount
+                clear foundList;
+                foundCount=0;
+                for q=1:pCount
+                    if(p==q)
+                        continue;
+                    end
+                    mainPlayer=playerCollection.list(p);                    
+                    otherPlayer=playerCollection.list(q);
+                    if(mainPlayer.startFrame+t~=otherPlayer.startFrame)
+                        continue;
+                    end
+                    
+                    
+                    isFound=1;
+                    foundCount=foundCount+1;
+                    foundList(foundCount)=q;
+                    return;
+                end
+                if(foundCount>0)
+                    display(strcat('main:',num2str(p),10,'possible:',num2str(foundList)));
+                end
+            end            
+        end
+    end
 end
 
